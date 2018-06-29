72d8ea04bce6959371cb7b130d3e8c2cbc146d53
Ron Clark <ronaldtclark@gmail.com>
2018-06-29 16:55:52 -0500

    readme edited

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # project_1-the_game
+
+TEXAS HOLD 'EM
+
+2 players:
+  1 user
+  1 cpu
+
+Round:
+ante bet collected
+each player is given 2 cards
+  round of betting
+three community cards dealt "the flop"
+  round of betting
+one community card dealt "the turn"
+  round of betting
+one community card dealt "the river"
+  round of betting
+showdown occurs if both players still active
+if either player folds during any round of betting, round ends immediately
+
+at showdown, 5 cards automatically chosen from 7 possible cards to make the highest hand type possible. 
+computer will:
+  organize the 5 cards
+    matching values first
+    followed by highest to lowest values
+  compare the hands and declare a winner
+winner receives the pot (consists of all bets made during the round)
+
+hand types in order:
+  royal flush
+  straight flush
+  4 of a kind
+  full house
+  flush
+  straight
+  3 of a kind
+  two pair
+  pair
+  high card
+
+a higher hand type beats any lower hand type.
+if both players have same hand type, the computer iterates over the hands comparing each card one at a time looking for tie breaker.
+
+each card represented by two characters:
+  a value [A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2]
+  a suit [C, S, H, D]
+  example: AH = ace of hearts, TD = ten of diamonds
+
--- js/app.js
+++ js/app.js
@@ -25,40 +25,6 @@ const player = {
 
 
 
-const handType = {
-  straightFlush = {
-    // card 1, card 2, card 3, card 4, card 5 all consecutive .value indexes
-    // card 1, card 2, card 3, card 4, card 5 all same suit
-    // tie break: card 1 comparison
-  } 
-  fourOfAKind = {
-    // card 1, card 2, card 3, card 4, all same value 
-    // card 5 negligibile. 
-    // tie break: card 1 comparison 
-  }
-  fullHouse = {
-    // card 1, card 2, card 3 all same value. card 4, card 5 same value.
-    // tie break: card 1 comparison, then card 4 comparison 
-  }
-  straight = {
-    // card 1, card 2, card 3, card 4, card 5 all consecutive .value index
-    // tie break: card 1 comparison 
-  }
-  threeOfAKind = {
-    // card 1, card 2, card 3 all same value. card 4, card 5 different values from the first 3 and each other.
-    // tie break: card 1 comparison, then card 4 comparison, then card 5 comparison
-  }
-  twoPair = {
-    // card 1, card 2 matching .value index. card 3, card 4 matching .value index different from first two. card 5 not the same value index as either set.
-    // tie break: card 1 comparison, then card 3 comparison, then card 5 comparison
-  }
-  pair = {
-    // card 1, card 2 matching .value index. card 3 different value index. card 4 different from set and card 3. card 5 different value index from rest of hand.
-    // tie break: card 1 comparison, then card 3 comparison, then card 4 comparison, then card 5 comparison.
-  }
-  highCard = {
-    // no match.                                          
-  }
 
 
 
@@ -71,10 +37,63 @@ const handType = {
 
 
 
+const gameObject = {
+
+    const handType = {
+      straightFlush: [];
+        // card 1, card 2, card 3, card 4, card 5 all consecutive .value indexes
+        // card 1, card 2, card 3, card 4, card 5 all same suit
+        // tie break: card 1 comparison
+      
+      fourOfAKind: [];
+        // card 1, card 2, card 3, card 4, all same value 
+        // card 5 negligibile. 
+        // tie break: card 1 comparison 
+      
+      fullHouse: [];
+        // card 1, card 2, card 3 all same value. card 4, card 5 same value.
+        // tie break: card 1 comparison, then card 4 comparison 
+
+      flush: [];
+        // card 1, card 2, card 3, card 4, card 5 all same suit index. 
+        // tie break: compare all cards 1-5 in order.
+      
+      straight: [];
+        // card 1, card 2, card 3, card 4, card 5 all consecutive .value index
+        // tie break: card 1 comparison 
+      
+      threeOfAKind: [];
+        // card 1, card 2, card 3 all same value. card 4, card 5 different values from the first 3 and each other.
+        // tie break: card 1 comparison, then card 4 comparison, then card 5 comparison
+      
+      twoPair: [];
+        // card 1, card 2 matching .value index. card 3, card 4 matching .value index different from first two. card 5 not the same value index as either set.
+        // tie break: card 1 comparison, then card 3 comparison, then card 5 comparison
+      
+      pair: [];
+        // card 1, card 2 matching .value index. card 3 different value index. card 4 different from set and card 3. card 5 different value index from rest of hand.
+        // tie break: card 1 comparison, then card 3 comparison, then card 4 comparison, then card 5 comparison.
+      
+      highCard: [];
+        // no value index matches, at least two suits.  
+        // tie break: compare all cards 1-5 in order.                                  
+      }
 
 
 
-}
+      
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
